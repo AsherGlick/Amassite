4d4d97e481134ec9cd5dd86112d346bfbb6b8bda
Asher Glick <asher.glick@gmail.com>
2012-05-21 14:55:42 -0400

    beginning to add user input features

--- amassite.py
+++ amassite.py
@@ -1,18 +1,28 @@
-import re
+#!/usr/bin/env python
 
+import re
+import sys
+
+
+##################################### MAIN #####################################
+# The main function, does all the work, all the time...
+################################################################################
 def main():
-  matchPattern = "{{.*?}}"
-  m = re.sub(matchPattern,parse_element,'{{helloworld var=tom}}{{a}}')
-  print m
+  print sys.argv
+  print len(sys.argv)
+
+def parsefile ( file_text ):
+  match_pattern = "{{.*?}}"
+  parsed_file = re.sub(match_pattern,parseelement,file_text)
 
 ################################# PARSE ELEMENT ################################
 # This function takes in a matched object tag and then parses the insides to   #
 # result in the valid HTML for the compiled file                               #
 ################################################################################
-def parse_element (matchobj):
+def parseelement (matchobj):
   text = matchobj.group(0)
-  print text
   return "!"+text+"!"
-  
+
+
 if __name__ == '__main__':
   main()
