9e5aeb0a70528e454c21ad4f619a211c049d1b86
Asher Glick <asher.glick@gmail.com>
2013-06-17 01:33:18 -0400

    converted tabs to spaces in unit test file

--- tests/unitTest.py
+++ tests/unitTest.py
@@ -3,78 +3,78 @@ import subprocess
 import os
 
 units = [
-	{"TestName" : "Single Var Arg (Generate)"
-	,"Test"     : "../source/amassite.py -c htmlvars/varArgument.html output.html"
-	,"Stdin"    : ""
-	,"Result"   : "Cleanup Mode\n"
-	}
-	,
-	{"TestName" : "Single Var Arg (Compare)"
-	,"Test"     : "cat output.html"
-	,"Stdin"    : ""
-	,"Result"   : open("htmlvars/results/varArgument.html").read()
-	}
-	,
-	{"TestName" : "Array Argument (Generate)"
-	,"Test"     : "../source/amassite.py -c htmlvars/arrayArgument.html output.html"
-	,"Stdin"    : ""
-	,"Result"   : "Cleanup Mode\n"
-	}
-	,
-	{"TestName" : "Array Argument (Compare)"
-	,"Test"     : "cat output.html"
-	,"Stdin"    : ""
-	,"Result"   : open("htmlvars/results/arrayArgument.html").read()
-	}
-	,
-	{"TestName" : "Nested Var Argument (Generate)"
-	,"Test"     : "../source/amassite.py -c htmlvars/nestedVarArgument.html output.html"
-	,"Stdin"    : ""
-	,"Result"   : "Cleanup Mode\n"
-	}
-	,
-	{"TestName" : "Nested Var Argument (Compare)"
-	,"Test"     : "cat output.html"
-	,"Stdin"    : ""
-	,"Result"   : open("htmlvars/results/nestedVarArgument.html").read()
-	}
-	,
-	{"TestName" : "Indeneted Var Argument (Generate)"
-	,"Test"     : "../source/amassite.py -c htmlvars/indentedVarArgument.html output.html"
-	,"Stdin"    : ""
-	,"Result"   : "Cleanup Mode\n"
-	}
-	,
-	{"TestName" : "Indented Var Argument (Compare)"
-	,"Test"     : "cat output.html"
-	,"Stdin"    : ""
-	,"Result"   : open("htmlvars/results/indentedVarArgument.html").read()
-	}
-	,
-	{"TestName" : "Indeneted Array Argument (Generate)"
-	,"Test"     : "../source/amassite.py -c htmlvars/indentedArrayArgument.html output.html"
-	,"Stdin"    : ""
-	,"Result"   : "Cleanup Mode\n"
-	}
-	,
-	{"TestName" : "Indented Array Argument (Compare)"
-	,"Test"     : "cat output.html"
-	,"Stdin"    : ""
-	,"Result"   : open("htmlvars/results/indentedArrayArgument.html").read()
-	}
-	# Debugging output Tests
-	,
-	{"TestName" : "Main Doc With Undefined Variable"
-	,"Test"     : "../source/amassite.py -c debugOutput/docWithError.html output.html"
-	,"Stdin"    : ""
-	,"Result"   : "Cleanup Mode\nERROR: name 'helloworld' is not defined on line 6 of debugOutput/docWithError.html\n"
-	}
-	,
-	{"TestName" : "Template With Undefined Variable"
-	,"Test"     : "../source/amassite.py -c debugOutput/templateWithErrorWrapper.html output.html"
-	,"Stdin"    : ""
-	,"Result"   : "Cleanup Mode\nERROR: name 'error' is not defined on line 2 of templateWIthError.html\n"
-	}
+    {"TestName" : "Single Var Arg (Generate)"
+    ,"Test"     : "../source/amassite.py -c htmlvars/varArgument.html output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : "Cleanup Mode\n"
+    }
+    ,
+    {"TestName" : "Single Var Arg (Compare)"
+    ,"Test"     : "cat output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : open("htmlvars/results/varArgument.html").read()
+    }
+    ,
+    {"TestName" : "Array Argument (Generate)"
+    ,"Test"     : "../source/amassite.py -c htmlvars/arrayArgument.html output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : "Cleanup Mode\n"
+    }
+    ,
+    {"TestName" : "Array Argument (Compare)"
+    ,"Test"     : "cat output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : open("htmlvars/results/arrayArgument.html").read()
+    }
+    ,
+    {"TestName" : "Nested Var Argument (Generate)"
+    ,"Test"     : "../source/amassite.py -c htmlvars/nestedVarArgument.html output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : "Cleanup Mode\n"
+    }
+    ,
+    {"TestName" : "Nested Var Argument (Compare)"
+    ,"Test"     : "cat output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : open("htmlvars/results/nestedVarArgument.html").read()
+    }
+    ,
+    {"TestName" : "Indeneted Var Argument (Generate)"
+    ,"Test"     : "../source/amassite.py -c htmlvars/indentedVarArgument.html output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : "Cleanup Mode\n"
+    }
+    ,
+    {"TestName" : "Indented Var Argument (Compare)"
+    ,"Test"     : "cat output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : open("htmlvars/results/indentedVarArgument.html").read()
+    }
+    ,
+    {"TestName" : "Indeneted Array Argument (Generate)"
+    ,"Test"     : "../source/amassite.py -c htmlvars/indentedArrayArgument.html output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : "Cleanup Mode\n"
+    }
+    ,
+    {"TestName" : "Indented Array Argument (Compare)"
+    ,"Test"     : "cat output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : open("htmlvars/results/indentedArrayArgument.html").read()
+    }
+    # Debugging output Tests
+    ,
+    {"TestName" : "Main Doc With Undefined Variable"
+    ,"Test"     : "../source/amassite.py -c debugOutput/docWithError.html output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : "Cleanup Mode\nERROR: name 'helloworld' is not defined on line 6 of debugOutput/docWithError.html\n"
+    }
+    ,
+    {"TestName" : "Template With Undefined Variable"
+    ,"Test"     : "../source/amassite.py -c debugOutput/templateWithErrorWrapper.html output.html"
+    ,"Stdin"    : ""
+    ,"Result"   : "Cleanup Mode\nERROR: name 'error' is not defined on line 2 of templateWIthError.html\n"
+    }
 ]
 
 
@@ -90,14 +90,14 @@ units = [
 
 
 def run (command, cin=""):
-	commands = command.split(" ")
-	process = Popen(commands, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-	if cin != "":
-		output = process.communicate(input=cin)[0]
-	else:
-		exit_code = os.waitpid(process.pid, 0)
-		output = process.communicate()[0]
-	return output
+    commands = command.split(" ")
+    process = Popen(commands, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    if cin != "":
+        output = process.communicate(input=cin)[0]
+    else:
+        exit_code = os.waitpid(process.pid, 0)
+        output = process.communicate()[0]
+    return output
 
 
 
@@ -140,10 +140,8 @@ def getTerminalSize():
 
 (width, height) = getTerminalSize()
 for unit in units:
-	if run(unit["Test"],unit["Stdin"]) == unit["Result"]:
-		print unit["TestName"] + " " +("."*(width-11-len(unit["TestName"])) ) + OKGREEN + " [SUCESS]" + ENDC
-	else:
-		print unit["TestName"] + " " +("."*(width-11-len(unit["TestName"])) ) + FAIL + " [FAILED]" + ENDC
-		print run(unit["Test"],unit["Stdin"])
-
-
+    if run(unit["Test"],unit["Stdin"]) == unit["Result"]:
+        print unit["TestName"] + " " +("."*(width-11-len(unit["TestName"])) ) + OKGREEN + " [SUCESS]" + ENDC
+    else:
+        print unit["TestName"] + " " +("."*(width-11-len(unit["TestName"])) ) + FAIL + " [FAILED]" + ENDC
+        print run(unit["Test"],unit["Stdin"])
